c2edf57b072e7c0351171d0136083abf3bad2567
João Massan <joaomassan@gmail.com>
2020-02-28 21:21:53 -0300

    Versão com o case (sem funcionar)

--- cap04/users.sh
+++ cap04/users.sh
@@ -12,18 +12,31 @@
 # v1.1		adds support to -V (version)
 ##############################################################################
 
+CURRENT_VERSION="v1.1"
+
 USE_MESSAGE="
-Use $0	[-h]
+Use $0 [-h]
 
   -h	shows help
   -V	shows current version
 
 "
 
-if test "$1" = "-h"
-then
-  echo "$USE_MESSAGE"
-  exit 0
-fi
+case "$1" in
+  -h)
+    echo $USE_MESSAGE
+    exit 0
+  ;;
+
+  -V)	
+    echo $CURRENT_VERSION
+    exit 0
+  ;;
+
+   *)
+     echo "invalid option: $1"
+     exit 1
+  ;;
+esac
 
 cut -d : -f 1,5 /etc/passwd | tr : \\t\\t
